b5b8c97d124d5c75f00e8924e645f26a02b32980
fsm.systems@gmail.com <fsm.systems@gmail.com>
2020-06-12 11:04:40 +0200

    modified:   pyping.py

--- pyping.py
+++ pyping.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-import subprocess,requests,platform
+import subprocess,requests,platform,time,socket
 from influxdb import InfluxDBClient
 
 #pip install influxdb
 #python3-influxdb
 
-#InfluxDB Parameters
+# InfluxDB Parameters
 influxdb_host='192.168.1.5'
 port=8086
 dbname='pyping'
 
-#Ping host
+# Ping host
 hostping=['8.8.8.8','1.1.1.1']
 
-#http checks
+# http checks
 http_urls=['https://www.google.es','https://lo.caixabank.es','http://www.tintaroja.cat','https://m.caixabank.es']
 timeout=60
 
@@ -22,11 +22,38 @@ location=platform.node()
 
 print('Testing from: ' +location)
 
-for host in hostping:
-   client = InfluxDBClient(host=influxdb_host, port=port)
-   client.create_database('pyping')
-   client.switch_database('pyping')
+# resolvehost
+resolvehost=['www.caixabank.es','www.google.es']
 
+
+# Influxdb Connection
+client = InfluxDBClient(host=influxdb_host, port=port)
+client.create_database('pyping')
+client.switch_database('pyping')
+
+for r in resolvehost:
+   dns_start = time.time()
+   ip_address = socket.gethostbyname(r)
+   dns_end = time.time()
+   dns_dif = dns_end - dns_start
+   print('Resolving: '+r+' in: ' + str(dns_dif) +'')
+   dnsEvent = [{"measurement":"dns",
+           "tags": {
+               "Location": location,
+               "RemoteHost": r,
+
+           },
+           "fields":
+           {
+           "dnstime":dns_dif
+           }
+           }
+           ]
+
+   client.write_points(dnsEvent)
+
+
+for host in hostping:
    ping_response = subprocess.Popen(["/bin/ping", "-c1", host], stdout=subprocess.PIPE).stdout.read()
    ping_parse_time = float(str(ping_response).split('time=')[1].split(' ms')[0])
    print('ping: '+host+' time: ' + str(ping_parse_time) +' ms')
